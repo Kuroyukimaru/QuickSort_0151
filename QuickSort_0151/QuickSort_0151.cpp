eba54acd7e5009aa5cea1103aaef3b8331a44305
Kuroyukimaru <oranggabut712@gmail.com>
2024-03-28 10:34:36 +0700

    membuat do-while di integer main

--- QuickSort_0151/QuickSort_0151.cpp
+++ QuickSort_0151/QuickSort_0151.cpp
@@ -88,3 +88,20 @@ void display() {
 }
 
 int main() {
+    char ch;
+
+    do {
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\nDo you to continue ? (y/n) :";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+        system("pause");
+        system("cls");
+    } while (true);
+
+    return 0;
+}
